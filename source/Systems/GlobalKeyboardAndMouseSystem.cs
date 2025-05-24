f2a19f6185a3978d629926771ae81b77c638c5de
Phill <philldamaskin@gmail.com>
2025-05-23 20:17:11 -0400

    optimization

--- source/Systems/GlobalKeyboardAndMouseSystem.cs
+++ source/Systems/GlobalKeyboardAndMouseSystem.cs
@@ -20,9 +20,24 @@ namespace InputDevices.Systems
         private static Vector2 globalMousePosition;
         private static Vector2 globalMouseScroll;
 
+        private double time;
+        private readonly int keyboardType;
+        private readonly int mouseType;
+        private readonly int globalTagType;
+        private readonly int timestampType;
+
         private TaskPoolGlobalHook? kbmHook;
         private uint globalKeyboardEntity;
         private uint globalMouseEntity;
+
+        public GlobalKeyboardAndMouseSystem(Simulator simulator)
+        {
+            Schema schema = simulator.world.Schema;
+            keyboardType = schema.GetComponentType<IsKeyboard>();
+            mouseType = schema.GetComponentType<IsMouse>();
+            globalTagType = schema.GetTagType<IsGlobal>();
+            timestampType = schema.GetComponentType<LastDeviceUpdateTime>();
+        }
 
         public void Dispose()
         {
@@ -34,6 +49,7 @@ namespace InputDevices.Systems
 
         void ISystem.Update(Simulator simulator, double deltaTime)
         {
+            time += deltaTime;
             World world = simulator.world;
             FindGlobalDevices(world);
             UpdateStates(world);
@@ -43,34 +59,30 @@ namespace InputDevices.Systems
         {
             globalKeyboardEntity = default;
             globalMouseEntity = default;
-            int keyboardType = world.Schema.GetComponentType<IsKeyboard>();
-            int mouseType = world.Schema.GetComponentType<IsMouse>();
-            int globalTagType = world.Schema.GetTagType<IsGlobal>();
             foreach (Chunk chunk in world.Chunks)
             {
                 Definition definition = chunk.Definition;
-                if (definition.ContainsComponent(keyboardType) && definition.ContainsTag(globalTagType))
+                if (definition.ContainsTag(globalTagType))
                 {
-                    if (chunk.Count > 1)
+                    if (definition.ContainsComponent(keyboardType))
                     {
-                        throw new InvalidOperationException("Multiple global keyboard entities found");
+                        if (globalKeyboardEntity != default)
+                        {
+                            throw new InvalidOperationException("Multiple global keyboard entities found");
+                        }
+
+                        globalKeyboardEntity = chunk.Entities[0];
                     }
 
-                    globalKeyboardEntity = chunk.Entities[0];
-                }
-            }
-
-            foreach (Chunk chunk in world.Chunks)
-            {
-                Definition definition = chunk.Definition;
-                if (definition.ContainsComponent(mouseType) && definition.ContainsTag(globalTagType))
-                {
-                    if (chunk.Count > 1)
+                    if (definition.ContainsComponent(mouseType))
                     {
-                        throw new InvalidOperationException("Multiple global mouse entities found");
-                    }
+                        if (globalMouseEntity != default)
+                        {
+                            throw new InvalidOperationException("Multiple global mice entities found");
+                        }
 
-                    globalMouseEntity = chunk.Entities[0];
+                        globalMouseEntity = chunk.Entities[0];
+                    }
                 }
             }
 
@@ -102,70 +114,108 @@ namespace InputDevices.Systems
         {
             if (globalKeyboardEntity != default)
             {
-                Keyboard keyboard = Entity.Get<Keyboard>(world, globalKeyboardEntity);
+                ref IsKeyboard component = ref world.GetComponent<IsKeyboard>(globalKeyboardEntity, keyboardType);
                 bool keyboardUpdated = false;
                 for (int i = 0; i < KeyboardState.MaxKeyCount; i++)
                 {
                     bool next = globalCurrentKeyboard[i];
                     bool previous = globalLastKeyboard[i];
-                    ButtonState state = keyboard.GetButtonState(i);
+                    ButtonState state = new(component.currentState[i], component.lastState[i]);
                     ButtonState current = new(previous, next);
                     if (state != current)
                     {
-                        globalLastKeyboard[i] = next;
-                        keyboard.SetButtonState(i, current);
                         keyboardUpdated = true;
+                        globalLastKeyboard[i] = next;
+                        if (current.value == ButtonState.State.Held)
+                        {
+                            component.currentState[i] = true;
+                            component.lastState[i] = true;
+                        }
+                        else if (current.value == ButtonState.State.WasPressed)
+                        {
+                            component.currentState[i] = true;
+                            component.lastState[i] = false;
+                        }
+                        else if (current.value == ButtonState.State.WasReleased)
+                        {
+                            component.currentState[i] = false;
+                            component.lastState[i] = true;
+                        }
+                        else
+                        {
+                            component.currentState[i] = false;
+                            component.lastState[i] = false;
+                        }
                     }
                 }
 
                 if (keyboardUpdated)
                 {
-                    DateTimeOffset when = DateTimeOffset.Now;
-                    TimeSpan timestamp = when - DateTimeOffset.UnixEpoch;
-                    InputDevice device = keyboard;
-                    device.SetUpdateTime(timestamp);
+                    ref LastDeviceUpdateTime timestamp = ref world.GetComponent<LastDeviceUpdateTime>(globalKeyboardEntity, timestampType);
+                    timestamp.time = time;
                 }
             }
 
             if (globalMouseEntity != default)
             {
-                Mouse mouse = new Entity(world, globalMouseEntity).As<Mouse>();
+                ref IsMouse component = ref world.GetComponent<IsMouse>(globalMouseEntity, mouseType);
                 bool mouseUpdated = false;
                 for (uint i = 0; i < MouseState.MaxButtonCount; i++)
                 {
                     bool next = globalCurrentMouse[i];
                     bool previous = globalLastMouse[i];
-                    ButtonState state = mouse.GetButtonState(i);
+                    ButtonState state = new(component.currentState[i], component.lastState[i]);
                     ButtonState current = new(previous, next);
                     if (state != current)
                     {
-                        globalLastMouse[i] = next;
-                        mouse.SetButtonState(i, current);
                         mouseUpdated = true;
+                        globalLastMouse[i] = next;
+                        if (current.value == ButtonState.State.Held)
+                        {
+                            component.currentState[i] = true;
+                            component.lastState[i] = true;
+                        }
+                        else if (current.value == ButtonState.State.WasPressed)
+                        {
+                            component.currentState[i] = true;
+                            component.lastState[i] = false;
+                        }
+                        else if (current.value == ButtonState.State.WasReleased)
+                        {
+                            component.currentState[i] = false;
+                            component.lastState[i] = true;
+                        }
+                        else
+                        {
+                            component.currentState[i] = false;
+                            component.lastState[i] = false;
+                        }
                     }
                 }
 
                 if (globalMouseMoved)
                 {
-                    ref Vector2 currentPosition = ref mouse.Position;
+                    ref Vector2 currentPosition = ref component.currentState.position;
+                    ref Vector2 currentDelta = ref component.currentState.delta;
+                    component.lastState.position = currentPosition;
                     Vector2 delta = globalMousePosition - currentPosition;
-                    mouse.Delta = delta;
                     currentPosition = globalMousePosition;
+                    currentDelta = delta;
                     mouseUpdated = true;
                 }
 
                 if (globalMouseScrolled)
                 {
-                    mouse.Scroll = globalMouseScroll;
+                    ref Vector2 currentScroll = ref component.currentState.scroll;
+                    component.lastState.scroll = currentScroll;
+                    currentScroll = globalMouseScroll;
                     mouseUpdated = true;
                 }
 
                 if (mouseUpdated)
                 {
-                    DateTimeOffset when = DateTimeOffset.Now;
-                    TimeSpan timestamp = when - DateTimeOffset.UnixEpoch;
-                    InputDevice device = mouse;
-                    device.SetUpdateTime(timestamp);
+                    ref LastDeviceUpdateTime timestamp = ref world.GetComponent<LastDeviceUpdateTime>(globalMouseEntity, timestampType);
+                    timestamp.time = time;
                 }
 
                 globalMouseMoved = false;
